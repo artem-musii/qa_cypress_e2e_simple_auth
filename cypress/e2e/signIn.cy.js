2a9ddfc86e681c4637143686f383b3c9cc618c1f
Artem Musii <musiy.artem@gmail.com>
2024-07-07 12:59:48 +0200

    added tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,18 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
-
+  it('should provide an ability to log in', () => {
+    cy.login('tomsmith', 'SuperSecretPassword!');
+    cy.url().should('include', 'secure');
   });
 
-  it('', () => {
+  it('should not allow to log in with invalid creds', () => {
+    cy.login('Invalid Username', 'Invalid Password');
+    cy.get('.error').should('exist');
+  });
 
+  it('should allow to logout', () => {
+    cy.login('tomsmith', 'SuperSecretPassword!');
+    cy.get('[href="/logout"]').click();
   });
 });
